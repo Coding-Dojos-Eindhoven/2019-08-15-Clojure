d1d89bf1267782edd5bd5e3785f09f08f2a160ef
Stefan van den Oord <svdo@users.noreply.github.com>
2019-08-13 13:48:40 +0200

    Shows how to handle immutability

--- src/tic_tac_toe/game.clj
+++ src/tic_tac_toe/game.clj
@@ -17,8 +17,16 @@
   ;; `assoc-in` to update the data structure defined above:
   (def board0 empty-board)      ; define a var called `board0` that is set to `empty-board`
   (assoc-in board0 [1 1] :X)    ; set the element in row 1 col 1 to :X
-  (print-board board0))         ; print the board
+  (print-board board0)          ; print the board
 
   ;; EXERCISE: look at the printed output and try to explain it.
 
+  ;; What happened? The board didn't change! That's because of _immutability_.
+  ;; In functional programming, all data is immutable. It cannot change, ever.
+  ;; But that doesn't make sense, we need the board to change!
+
+  ;; No worries: instead of modifying the board given to it, `assoc-in` returns
+  ;; a _copy_ of the board that contains the desired changes.
+  (def board1 (assoc-in empty-board [1 1] :X))
+  (print-board board1))
 
