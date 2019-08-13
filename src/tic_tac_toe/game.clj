45cb09192b1e48ae8094a6e857f5be45e2011bc9
Stefan van den Oord <svdo@users.noreply.github.com>
2019-08-13 13:43:14 +0200

    Uses `assoc-in` for updating the board

--- src/tic_tac_toe/game.clj
+++ src/tic_tac_toe/game.clj
@@ -10,6 +10,15 @@
 
 (comment
   ;; We have a utility to nicely print those boards
-  (print-board empty-board))
+  (print-board empty-board)
+
+  ;; Building blocks. One thing we'll need to use while playing, is
+  ;; updating the board according to the players' moves. We can use
+  ;; `assoc-in` to update the data structure defined above:
+  (def board0 empty-board)      ; define a var called `board0` that is set to `empty-board`
+  (assoc-in board0 [1 1] :X)    ; set the element in row 1 col 1 to :X
+  (print-board board0))         ; print the board
+
+  ;; EXERCISE: look at the printed output and try to explain it.
 
 
